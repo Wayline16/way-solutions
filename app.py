8bf57574236322b3164a13868db7390570400b3b
Wayline16 <120781316+Wayline16@users.noreply.github.com>
2024-02-09 12:10:44 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hellow_world():
-  return "<p>Hello, Wayline!</p>"
+def hello_wayline():
+  return render_template('home.html')
 
 
 print(__name__)

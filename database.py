aa4957ac134c956d3c6ae442eef2081d5ecc4f97
Wayline Jeffries <120781316+Wayline16@users.noreply.github.com>
2024-03-05 12:35:22 +0000

    Store application form in database

--- database.py
+++ database.py
@@ -37,5 +37,11 @@ def load_job_from_db(id):
           print("No results found.")
 
       return job[0]
-    
+
+def add_application_to_db(job_id, application):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)").params(job_id=job_id,full_name=application['full_name'],email=application['email'],linkedin_url=application['linkedin_url'],education=application['education'],work_experience=application['work_experience'],resume_url=application['resume_url'])
+
+    conn.execute(query)
+
     
